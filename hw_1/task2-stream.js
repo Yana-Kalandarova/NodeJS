efb089708ca35f0ab469842838b5f7b0c17edc14
Yana_Kalandarava <yana.kalandarava@epicgames.com>
2021-06-04 16:05:07 +0300

    [HW_1]: task-2 (read file to RAM)

--- hw_1/task2-stream.js
+++ hw_1/task2-stream.js
@@ -2,7 +2,7 @@ import csvtojson from 'csvtojson';
 import fs from 'fs';
 
 const inputFile = './csv/task-2.csv';
-const outputFile = './txt/task-2.txt';
+const outputFile = './txt/task-2-stream.txt';
 const readable = fs.createReadStream(inputFile, 'utf8');
 const writable = fs.createWriteStream(outputFile, 'utf8');
 
@@ -10,7 +10,6 @@ csvtojson()
 	.fromStream(readable)
 	.subscribe(
 		(jsonItem) => {
-			console.log(jsonItem);
 			writable.write(JSON.stringify(jsonItem).concat('\n'), 'utf8');
 		},
 		(error) => error && console.log(error),
