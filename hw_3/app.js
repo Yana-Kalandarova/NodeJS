6d805e7945ba7038d194d8d8b1400143772b7858
Yana_Kalandarava <yana.kalandarava@epicgames.com>
2021-07-03 20:56:21 +0300

    [HW_5]: add logger for uncaughtException and unhandledRejection

--- hw_3/app.js
+++ hw_3/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { usersRouter, groupsRouter } from './routes';
 import { requestLogger } from './middleware';
+import { logger } from './utils/logger';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,5 +11,11 @@ app.use(requestLogger);
 
 app.use('/users', usersRouter);
 app.use('/groups', groupsRouter);
+
+process
+    .on('uncaughtException', (err) => logger.error(`uncaughtException: ${err}`))
+    .on('unhandledRejection', (err) =>
+        logger.error(`unhandledRejection: ${err}`)
+    );
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
